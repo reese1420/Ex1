42e20c0e0e2a809b9218d3b010c37383129fa762
reese1420 <tj.espanol97@gmail.com>
2019-11-11 12:58:06 -0800

    first commit

---
+++ src/Ex1.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Ex1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Hi Aly" << endl;
+
+	return 0;
+}
